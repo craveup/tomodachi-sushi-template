d5b91a492a2c19a4946b819cb416850a88ba88b1
Muhammad Umar Farooq <umar@craveup.com>
2025-09-03 21:17:11 +0500

    restaurant theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,12 +59,14 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${forum.variable} ${yujiMai.variable} ${wdxlLubrifont.variable} antialiased`}
       >
         <ErrorBoundary>
-            <AddressProvider>
+          <RestaurantThemeProvider defaultThemePath="/themes/leclerc-theme.json">
+          <AddressProvider>
               <CartProvider>
                 {children}
                 <CartWrapper />
               </CartProvider>
             </AddressProvider>
+          </RestaurantThemeProvider>
         </ErrorBoundary>
       </body>
     </html>
