eb9052043b67b53fc0f5b0cc0ada14c6f0268a3c
Muhammad Umar Farooq <umar@craveup.com>
2025-10-24 13:38:20 +0500

    chore: drop unused API helpers and tidy dependencies

--- src/lib/api/location.ts
+++
@@ -1,9 +0,0 @@
-// /app/lib/api/location.ts
-import { GetLocationViaSlugType } from "@/types/location-types";
-import { apiFetch } from "./fetcher";
-
-export async function getLocationById(
-  locationId: string
-): Promise<GetLocationViaSlugType> {
-  return apiFetch(`/api/v1/locations/${locationId}`);
-}
